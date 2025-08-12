43f5361698f08c25f302b4bcce28bb2120663026
ram23sarath <38622885+ram23sarath@users.noreply.github.com>
2025-08-12 15:50:04 +0000

    Made changes to code

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-let myLeads = ["Jaime", "Nolan", "Denis"]
+let myLeads = []
 let inputEl = document.getElementById("input-el")
 const inputButton = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputButton.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderData()
 })
 
-listItems = ""
+function renderData() {
+    let listItems = ""
 
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + "<a target = '_blank' href= " + myLeads[i] + ">" + myLeads[i] + "</a></li>"
+    }
+
+    ulEl.innerHTML = listItems
 }
-
-ulEl.innerHTML = listItems
